1f35c9d01777f3afb13df64bded85eeec41a9b6e
Luke Snape <ldjsnape@gmail.com>
2017-10-10 14:00:30 +0100

    Refactor fingerprint namespace, improve tests

--- src/elit/fingerprint.clj
+++ src/elit/fingerprint.clj
@@ -5,40 +5,46 @@
 
 (def asset-regex #"\$\{(.+?)\}")
 (def file-path-regex #"(.*)\.([^.]*?)$")
-(def seperator-char (first (File/separator)))
+(def separator-char (first (File/separator)))
+(def leading-slash-regex (re-pattern (str "^" separator-char)))
+
+(defn add-leading-slash
+  [path]
+  (cond->> path
+    (not= separator-char (first path))
+    (str separator-char)))
+
+(defn remove-leading-slash
+  [path]
+  (string/replace-first path leading-slash-regex ""))
+
+(defn add-trailing-slash
+  [path]
+  (cond-> path
+    (not= separator-char (last path))
+    (str separator-char)))
 
 (defn fingerprint-file-path
   [path hash]
   (string/replace path file-path-regex (str "$1-" hash ".$2")))
 
-(defn drop-trailing-slash
-  [path]
-  (apply str (cond->> path
-               (= seperator-char (last path))
-               butlast)))
-
-(defn with-leading-slash
-  [path]
-  (cond->> path
-    (not= seperator-char (first path))
-    (str seperator-char)))
+(defn remove-asset-root
+  [asset-ref asset-root]
+  (string/replace-first asset-ref
+                        (re-pattern (str asset-root seperator-char)) ""))
 
 (defn prepend-asset-host
-  [asset-host]
-  (if asset-host
-    #(str (drop-trailing-slash asset-host) %)
-    identity))
-
-(defn remove-asset-root
-  [asset-root]
-  (if asset-root
-    #(string/replace-first % (re-pattern (str asset-root seperator-char)) "")
-    identity))
+  [asset-ref {:keys [asset-host asset-root]}]
+  (cond->> asset-ref
+    asset-host
+    (str (add-trailing-slash asset-host))))
 
 (defn normalise-asset-ref
   [asset-ref asset-root]
-  (cond->> (with-leading-slash asset-ref)
-    asset-root (str asset-root)))
+  (if asset-root
+    (->> (add-leading-slash asset-ref)
+         (str asset-root))
+    (remove-leading-slash asset-ref)))
 
 (defn find-asset-refs
   [file-text {:keys [asset-root]}]
@@ -46,23 +52,23 @@
        (map (comp #(normalise-asset-ref % asset-root)
                   second))))
 
-(defn replacer-fn
-  [{:keys [asset-root asset-host path->file skip? strict?]}]
-  (fn [match]
-    (if-not skip?
-      (if-let [{:keys [hash]} (get path->file (normalise-asset-ref match asset-root))]
-        ((comp (prepend-asset-host asset-host)
-               (remove-asset-root asset-root)
-               #(fingerprint-file-path % hash))
-         match)
-        (if strict?
-          (throw
-           (ex-info "Could not find asset in fileset"
-                    {:match match}))
-          match))
-      match)))
+(defn replacer
+  [path {:keys [asset-root path->file skip? strict?] :as opts}]
+  (if-not skip?
+    (if-let [{:keys [hash]} (->> (normalise-asset-ref path asset-root)
+                                 (get path->file))]
+      (-> path
+          (fingerprint-file-path hash)
+          (remove-leading-slash)
+          (prepend-asset-host opts))
+      (if strict?
+        (throw
+         (ex-info "Could not find asset in fileset"
+                  {:path path}))
+        path))
+    path))
 
 (defn update-text
   [file-text opts]
-  (string/replace file-text asset-regex (comp (replacer-fn opts)
+  (string/replace file-text asset-regex (comp #(replacer % opts)
                                               second)))
--- test/src/elit/fingerprint_test.clj
+++ test/src/elit/fingerprint_test.clj
@@ -4,9 +4,13 @@
 
 (deftest detecting-asset-paths
   (testing "empty reference is ignored"
-    (is (empty? (sut/find-asset-refs "foo ${} bar"))))
+    (is (empty? (sut/find-asset-refs "foo ${} bar" {}))))
   (testing "non-empty reference is detected"
-    (is (= ["baz"] (sut/find-asset-refs "foo ${baz} bar")))))
+    (is (= ["baz"] (sut/find-asset-refs "foo ${baz} bar" {}))))
+  (testing "leading slash is stripped"
+    (is (= ["baz"] (sut/find-asset-refs "foo ${/baz} bar" {}))))
+  (testing "asset root is added"
+    (is (= ["public/baz"] (sut/find-asset-refs "foo ${/baz} bar" {:asset-root "public"})))))
 
 (deftest fingerprinting-file-paths
   (testing "hash is appended to filename before the suffix"
@@ -14,30 +18,31 @@
            "path/to/file-123.ext"))))
 
 (deftest replacing-references-to-assets
-  (let [match "/foo/bar.ext"
+  (let [rel-path "bar/baz.ext"
+        abs-path (str "foo/" rel-path)
         hash 123]
-    (testing "matched reference to asset is replaced"
-      (let [replacer (sut/replacer-fn {:path->file {match {:hash hash}}})]
-        (is (= (replacer match)
-               (sut/fingerprint-file-path match hash)))))
+    (testing "pathed reference to asset is replaced"
+      (is (= (sut/replacer abs-path {:path->file {abs-path {:hash hash}}})
+             (sut/fingerprint-file-path abs-path hash))))
     (testing "no-op if skip? is true"
-      (let [replacer (sut/replacer-fn {:path->file {match {:hash hash}}
-                                       :skip? true})]
-        (is (= (replacer match)
-               match))))
-    (testing "asset root is removed if provided"
-      (let [replacer (sut/replacer-fn {:asset-root "foo"
-                                       :path->file {match {:hash hash}}})]
-        (is (= (replacer match)
-               "/bar-123.ext"))))
+      (is (= (sut/replacer abs-path {:path->file {abs-path {:hash hash}}
+                                     :skip? true})
+             abs-path)))
     (testing "asset host is prepended if provided"
-      (let [replacer (sut/replacer-fn {:asset-host "https://my.cdn/assets/"
-                                       :path->file {match {:hash hash}}})]
-        (is (= (replacer match)
-               "https://my.cdn/assets/foo/bar-123.ext"))))
+      (is (= (sut/replacer abs-path {:asset-host "https://my.cdn/assets/"
+                                     :path->file {abs-path {:hash hash}}})
+             "https://my.cdn/assets/foo/bar/baz-123.ext")))
+    (testing "asset root is applied correctly"
+      (is (= (sut/replacer rel-path {:asset-root "foo"
+                                     :path->file {abs-path {:hash hash}}
+                                     :strict? true})
+             "bar/baz-123.ext")))
     (testing "options work together"
-      (let [replacer (sut/replacer-fn {:asset-host "https://my.cdn/assets"
-                                       :asset-root "foo"
-                                       :path->file {match {:hash hash}}})]
-        (is (= (replacer match)
-               "https://my.cdn/assets/bar-123.ext"))))))
+      (is (= (sut/replacer rel-path {:asset-host "https://my.cdn/assets"
+                                     :asset-root "foo"
+                                     :path->file {abs-path {:hash hash}}
+                                     :strict? true})
+             "https://my.cdn/assets/bar/baz-123.ext")))
+    (testing "throws exception if strict? option is set and file not found"
+      (is (try (sut/replacer rel-path {:path->file {} :strict? true})
+               (catch Exception _ true))))))
