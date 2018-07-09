1bb4824d738dc127b46937ba6652b884c8efb1a6
Luke Snape <ldjsnape@gmail.com>
2018-07-09 09:50:04 +0100

    Add regex option to asset-fingerprint task...
    This is to prevent processing of vendor JS files: a source of bugs particularly
    in dev mode where the output files are split.

--- src/elit/boot_asset_fingerprint.clj
+++ src/elit/boot_asset_fingerprint.clj
@@ -33,17 +33,17 @@
   on Rails Asset Pipeline guide, segment \"What is Fingerprinting and
   Why Should I Care\" for a detailed explanation of why you want to do this.
   (http://guides.rubyonrails.org/asset_pipeline.html#what-is-fingerprinting-and-why-should-i-care-questionmark) "
-  [a asset-root        ROOT  str   "The root dir where the assets are served from"
-   e extensions        EXT   [str] "Add a file extension to indicate the files to process for asset references"
-   o asset-host        HOST  str   "Host to prefix all asset urls with e.g. https://your-host.com"
-   s skip                    bool  "Skips file fingerprinting and replaces each asset reference with the unfingerprinted path"
-   t strict                  bool  "Throws an exception if an asset is not found in the fileset. Defaults to true"]
+  [a asset-root        ROOT  str     "The root dir where the assets are served from"
+   e extensions        EXT   [str]   "Add a file extension to indicate the files to process for asset references"
+   r regexes           RES   [regex] "A list of regexes for filtering file paths"
+   o asset-host        HOST  str     "Host to prefix all asset urls with e.g. https://your-host.com"
+   s skip                    bool    "Skips file fingerprinting and replaces each asset reference with the unfingerprinted path"
+   t strict                  bool    "Throws an exception if an asset is not found in the fileset. Defaults to true"]
   (let [out-dir (boot/tmp-dir!)]
     (boot/with-pre-wrap fileset
       (let [files (boot/input-files fileset)]
-        (asset-fingerprint* (boot/by-ext (or extensions
-                                             default-extensions)
-                                         files)
+        (asset-fingerprint* (cond->> (boot/by-ext (or extensions default-extensions) files)
+                              regexes (boot/by-re regexes))
                             out-dir
                             {:asset-host asset-host
                              :asset-root asset-root
